e7516f4e9b64360172e926e40ee453eb7930f3e3
praveen <praveenpawar414@gmail.com>
2021-09-04 23:54:32 +0530

    Day4 completed

---
+++ 04_String/string.py
@@ -0,0 +1,25 @@
+#Day 4: String - 30 Days of python programming
+
+#Exercise 4.1
+first_name='Praveen'
+last_name='Pawar'
+space=' '
+full_name=first_name + space + last_name
+print(full_name)
+
+#Exercise 4.2
+first_word='Python'
+second_word='For'
+third_word='All'
+print(first_name + space + second_word + space + third_word)
+
+#Exercise 4.3, 4.4, 4.5, 4.6, 4.7, 4.8
+company="Coding For All"
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company.swapcase())
+print(company.capitalize())
+print(company.title())
+
