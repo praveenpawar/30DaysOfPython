1452e12f745cc3af93dbac66b22be4866275c830
praveen <praveenpawar414@gmail.com>
2021-09-05 22:38:42 +0530

    Day5 completed

---
+++ 05_Day_List/list.py
@@ -0,0 +1,29 @@
+#Day5: List - 30 days of python learning
+
+#Exercise 5.1
+lst=list()
+print(lst)
+#Exercise 5.2
+fruits = ['banana', 'orange', 'mango', 'lemon']
+print(fruits)
+#Exercise 5.3
+print(len(fruits))
+#Exercise 5.4
+print(fruits[0])
+print(fruits[1:3])
+print(fruits[len(fruits)-1])
+print(fruits[-1])
+#Exercise 5.5 to 5.10
+mixed_data_types=['Praveen', 26, 178, 'single', 'India']
+it_companies=['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+print(mixed_data_types)
+print(it_companies)
+print(len(it_companies))
+print(it_companies[0])
+print(it_companies[-1])
+print(it_companies[3])
+it_companies.append('Cisco')
+print(it_companies)
+it_companies.insert(5, 'Dell')
+print(it_companies)
+
