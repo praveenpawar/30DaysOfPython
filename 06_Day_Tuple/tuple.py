8db09d85c8b78db5c27659de6aed528c6f62183e
praveen <praveenpawar414@gmail.com>
2021-09-06 23:17:23 +0530

    Day6 completed

---
+++ 06_Day_Tuple/tuple.py
@@ -0,0 +1,9 @@
+#Day 6: Tuple - 30 days of python programming
+
+#Exercise6.1
+tpl=tuple()
+print(tpl)
+#Exercise6.2
+cities=('Bhopal', 'Indore', 'Jabalpur', 'Gwalior')
+print(cities)
+print(len(cities))
