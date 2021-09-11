0a2d3ebc20d83e21dd0678420e2264eff6d049cd
praveen <praveenpawar414@gmail.com>
2021-09-11 23:27:54 +0530

    Day10 completed

---
+++ 10_Day_Loops/loops.py
@@ -0,0 +1,61 @@
+#Day 10: Loops - 30 days of python learning
+
+#Exercise10.1
+count=0
+while count < 11:
+    print(count)
+    count = count + 1
+
+#Exercise10.2
+count=10
+while count >= 0:
+    print(count)
+    count = count - 1
+
+#Exercise10.3
+count=0
+while count < 8:
+    print('#')
+    count = count + 1
+
+#Exercise10.4
+
+
+#Exercise10.5
+count=0
+while count < 11:
+    print('{} x {} = {}'.format(count, count, count*count))
+    count = count + 1
+
+#Exercise10.6
+lang = ['Python', 'Numpy','Pandas','Django', 'Flask']
+for language in lang:
+    print(language)
+
+#Exercise10.7 even number
+for num in range(0 , 101):
+    if num % 2 == 0:
+        print(num)
+
+#Exercise10.8 odd number
+for num in range(0 , 101):
+    if num % 2 != 0:
+        print(num)
+
+#Exercise10.9
+sum = 0
+for num in range(0, 101):
+    sum = sum + num
+
+print('The sum of all the numbers is {}'.format(sum))
+
+#Exercise10.10
+even_sum = 0
+odd_sum = 0
+for num in range(0, 101):
+    if num % 2 == 0:
+        even_sum = even_sum + num
+    else: 
+        odd_sum = odd_sum + num
+
+print('The sum of all evens is {}. And the sum of all odds is {}.'.format(even_sum, odd_sum))
