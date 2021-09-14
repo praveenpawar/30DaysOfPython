b32e7304a316ec028acd2cfe88a6808c4c7c18cd
praveen <praveenpawar414@gmail.com>
2021-09-14 23:13:57 +0530

    Day12 prog2 updated

--- 12_Day_Modules/modules.py
+++ 12_Day_Modules/modules.py
@@ -11,3 +11,17 @@ def random_user_id(length):
     
 random_user_id(6)
 
+#Exercise12.2
+import string
+import random
+length = int(input("Enter the number of the character in user_id: "))
+num = int(input("The number of IDs which are supposed to be generated: "))
+def random_user_id():
+    for i in range(num):
+        for j in range(length):
+            char = string.ascii_lowercase + string.digits
+            ran_user_id = random.choice(char)
+            print(ran_user_id, end='')
+        print()
+
+random_user_id()
